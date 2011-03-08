96bdb99f8d66feebfaf22bd970114fe66b6efe06
Gilbert <gilbertbgarza@gmail.com>
2011-03-08 17:24:52 -0600

    Added yield_for and content_for helpers

--- lib/staticmatic/helpers/render_helper.rb
+++ lib/staticmatic/helpers/render_helper.rb
@@ -4,6 +4,18 @@ module StaticMatic
     module RenderHelper
       self.extend self
       
+      def content_hash
+        @content_hash ||= {}
+      end
+      
+      def yield_for(index)
+        self.content_hash[index.to_sym] || ""
+      end
+      
+      def content_for(index,&block)
+        content_hash[:sidebar] = capture_haml &block
+      end
+      
       # Include a partial template
       def partial(name, options = {})
         @staticmatic.generate_partial(name, options)
