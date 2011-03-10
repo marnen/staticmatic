73c644106f36efedeee1678ae06e5ae8668c45f7
Gilbert <gilbertbgarza@gmail.com>
2011-03-10 11:08:12 -0600

    Added command to upload select files to Amazon S3, instead of the entire directory

--- lib/staticmatic/deployers/aws-s3.rb
+++ lib/staticmatic/deployers/aws-s3.rb
@@ -17,23 +17,11 @@ class StaticExe < Thor
       return
     end
     
-    say 'Uploading to Amazon S3...', :cyan
-    say 'Establishing connection to Amazon S3...'
     amazon_config = YAML::load File.read(amazon_config_path directory)
-    AWS::S3::Base.establish_connection!(amazon_config['AccessKeys'])
-    
     env = options[:env] || 'development'
     bucket_name = amazon_config['Buckets'][env]
     
-    say "Grabbing #{env} bucket '#{bucket_name}'"
-    buckets = AWS::S3::Service.buckets
-    unless buckets.map {|b| b.name}.include? bucket_name
-      say 'The bucket ', :red
-      say "'#{bucket_name}' "
-      say 'does not exist!', :red
-      say 'Nothing to do.'
-      return
-    end
+    connect_to_amazon_s3(amazon_config,env,bucket_name)
 
     build_files(directory).each do |file|
       contents = File.read file
@@ -45,7 +33,56 @@ class StaticExe < Thor
     say 'Done.'
   end
   
+  desc "s3_upload FILE [FILE ...]", "Upload select files to your Amazon S3 bucket."
+  method_option :directory, :type => :string, :aliases => "-d", :default => '.'
+  method_option :env, :type => :string, :aliases => "-e"
+  method_option :skip_build, :type => :boolean, :aliases => "-k"
+  def s3_upload(*files)
+    return if needs_setup? options[:directory]
+    
+    if files.length == 0
+      say 'No files selected.', :red
+      say 'Nothing to do.'
+      return
+    end
+    
+    amazon_config = YAML::load File.read(amazon_config_path '.')
+    env = options[:env] || 'development'
+    bucket_name = amazon_config['Buckets'][env]
+    
+    connect_to_amazon_s3(amazon_config,env,bucket_name)
+    
+    files.each do |file|
+      
+      unless File.exists? file
+        say "      Does not exist ", :red; say file; next
+      end
+      
+      contents = File.read file
+      s3path = file.sub(%r{^(\./)?build/},'')
+    
+      say "      upload ", :cyan; say s3path
+      AWS::S3::S3Object.store(s3path, contents, bucket_name, :access => :public_read)
+    end
+  end
+  
   private
+  
+  def connect_to_amazon_s3(config,env,bucket_name)
+    say 'Uploading to Amazon S3...', :cyan
+    say 'Establishing connection to Amazon S3...'
+    AWS::S3::Base.establish_connection!(config['AccessKeys'])
+    
+    say "Grabbing #{env} bucket '#{bucket_name}'"
+    buckets = AWS::S3::Service.buckets
+    unless buckets.map {|b| b.name}.include? bucket_name
+      say 'The bucket ', :red
+      say "'#{bucket_name}' "
+      say 'does not exist!', :red
+      say 'Nothing to do.'
+      return
+    end
+  end
   
   def amazon_config_path(directory)
     File.join(directory, 'config','amazon.yml')
