4b4108e67c80c5dbc5c501dd744d12681fcd4038
Gilbert <gilbertbgarza@gmail.com>
2011-09-16 03:12:50 -0500

    Fixed relative path bug

--- lib/staticmatic/helpers/current_path_helper.rb
+++ lib/staticmatic/helpers/current_path_helper.rb
@@ -11,7 +11,7 @@ module StaticMatic
 
       def current_page_relative_path(current_path = nil)
         if current_path.nil? || current_path.match(/^((\.\.?)?\/|\#|.+?\:)/) == nil
-          current_page_depth = current_page.split('/').length - 2;
+          current_page_depth = current_page.split('/').length - 3;
           (current_page_depth > 0) ? ([ '..' ] * current_page_depth).join('/') + '/' : ''
         else
           ''
