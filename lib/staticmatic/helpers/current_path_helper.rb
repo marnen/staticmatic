17db4a455395e6d8f67f493961c0d963d496863d
Gilbert <gilbertbgarza@gmail.com>
2011-09-16 12:36:44 -0500

    Fixed a bug where generated asset paths in a root-folder template were not relative.
    This was preventing built static sites from being viewed in the local filesystem.

--- lib/staticmatic/helpers/current_path_helper.rb
+++ lib/staticmatic/helpers/current_path_helper.rb
@@ -12,9 +12,9 @@ module StaticMatic
       def current_page_relative_path(current_path = nil)
         if current_path.nil? || current_path.match(/^((\.\.?)?\/|\#|.+?\:)/) == nil
           current_page_depth = current_page.split('/').length - 3;
-          (current_page_depth > 0) ? ([ '..' ] * current_page_depth).join('/') + '/' : ''
+          (current_page_depth > 0) ? ([ '..' ] * current_page_depth).join('/') + '/' : nil
         else
-          ''
+          nil
         end
       end
     end
